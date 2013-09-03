77d3baadc3ad2c504401383b40f89577c79c0f84
Gao.Yan <yioo_2007@126.com>
2013-09-03 09:29:10 +0800

    rm testx

--- project/TestX/c.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ndk-build
-cp ../XLib/libs/armeabi/libX.so libs/armeabi/
-echo "cp libX.so ==> libs/armeabi/libX.so"
--- project/TestX/jni/Application.mk
+++
@@ -1,7 +0,0 @@
-#APP_CPPFLAGS += -frtti
-
-APP_STL := gnustl_static
-
-APP_PLATFORM := android-9
-
-GCC_VER_SET :=4.3.3
