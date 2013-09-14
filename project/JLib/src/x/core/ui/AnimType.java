597f3337616b0a29bff6f280cba986c2bc0fad71
Gao.Yan <gaoyan@pset.suntect.net>
2013-09-14 17:43:23 +0800

    update anim

--- project/JLib/src/x/core/ui/AnimType.java
+++ project/JLib/src/x/core/ui/AnimType.java
@@ -1,17 +1,13 @@
 package x.core.ui;
 
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+
 public class AnimType {
-	//----------anim mask-----------------
-	public static final int MASK_LEFT_IN = 1;
-	public static final int MASK_LEFT_OUT = 2;
-	public static final int MASK_RIGHT_IN = 4;
-	public static final int MASK_RIGHT_OUT = 8;
-	public static final int MASK_TOP_IN = 16;
-	public static final int MASK_TOP_OUT = 32;
-	public static final int MASK_BOTTOM_IN = 64;
-	public static final int MASK_BOTTOM_OUT = 128;
-	
-	//----------anim type----------------------
+	/**
+	 * Anim type
+	 * Note: anim type must pairs appear
+	 */
 	public static final int A_NONE = 0;
 	public static final int A_LEFT_IN_RIGHT_OUT = 1;
 	public static final int A_RIGHT_IN_LEFT_OUT = 2;
@@ -19,7 +15,66 @@ public class AnimType {
 	public static final int A_TOP_IN_BOTTOM_OUT = 4;
 	
 	
-	public static final int getMask(int anType) {
+	//get the reverse anim's id
+	public static int getReverseAnim(int an) {
+		if (an == A_NONE) {
+			return A_NONE;
+		}
+		if ((an % 2) == 0) {
+			return an - 1;
+		}
+		return an + 1;
+	}
+	
+	public static Animation createInAnim(int animType) {
+		final int T = Animation.RELATIVE_TO_SELF;
+		final int M = 500;
+		int mask = getMask(animType);
+		TranslateAnimation in = null;
+		
+		if ((mask & AnimType.MASK_LEFT_IN) != 0) {
+			in = new TranslateAnimation(T, -1, T, 0, T, 0, T, 0);
+		}
+		if ((mask & AnimType.MASK_RIGHT_IN) != 0) {
+			in = new TranslateAnimation(T, 1, T, 0, T, 0, T, 0);
+		}
+		if ((mask & AnimType.MASK_TOP_IN) != 0) {
+			in = new TranslateAnimation(T, 0, T, 0, T, -1, T, 0);
+		}
+		if ((mask & AnimType.MASK_BOTTOM_IN) != 0) {
+			in = new TranslateAnimation(T, 0, T, 0, T, 1, T, 0);
+		}
+		if (in != null) {
+			in.setDuration(M);
+		}
+		return in;
+	}
+	
+	public static Animation createOutAnim(int animType) {
+		final int T = Animation.RELATIVE_TO_SELF;
+		final int M = 500;
+		int mask = getMask(animType);
+		TranslateAnimation out = null;
+		
+		if ((mask & AnimType.MASK_LEFT_OUT) != 0) {
+			out = new TranslateAnimation(T, 0, T, -1, T, 0, T, 0);
+		}
+		if ((mask & AnimType.MASK_RIGHT_OUT) != 0) {
+			out = new TranslateAnimation(T, 0, T, 1, T, 0, T, 0);
+		}
+		if ((mask & AnimType.MASK_TOP_OUT) != 0) {
+			out = new TranslateAnimation(T, 0, T, 0, T, 0, T, -1);
+		}
+		if ((mask & AnimType.MASK_BOTTOM_OUT) != 0) {
+			out = new TranslateAnimation(T, 0, T, 0, T, 0, T, 1);
+		}
+		if (out != null) {
+			out.setDuration(M);
+		}
+		return out;
+	}
+	
+	private static final int getMask(int anType) {
 		switch (anType) {
 		case A_LEFT_IN_RIGHT_OUT:
 			return MASK_LEFT_IN|MASK_RIGHT_OUT;
@@ -34,19 +89,13 @@ public class AnimType {
 		}
 	}
 	
-	//get the reverse anim's id
-	public static int getReverseAnim(int an) {
-		switch (an) {
-		case A_LEFT_IN_RIGHT_OUT:
-			return A_RIGHT_IN_LEFT_OUT;
-		case A_RIGHT_IN_LEFT_OUT:
-			return A_LEFT_IN_RIGHT_OUT;
-		case A_BOTTOM_IN_TOP_OUT:
-			return A_TOP_IN_BOTTOM_OUT;
-		case A_TOP_IN_BOTTOM_OUT:
-			return A_BOTTOM_IN_TOP_OUT;
-		default:
-			return A_NONE;
-		}
-	}
+	//----------anim mask-----------------
+	private static final int MASK_LEFT_IN = 1;
+	private static final int MASK_LEFT_OUT = 2;
+	private static final int MASK_RIGHT_IN = 4;
+	private static final int MASK_RIGHT_OUT = 8;
+	private static final int MASK_TOP_IN = 16;
+	private static final int MASK_TOP_OUT = 32;
+	private static final int MASK_BOTTOM_IN = 64;
+	private static final int MASK_BOTTOM_OUT = 128;
 }
--- project/JLib/src/x/core/ui/JAnimHelper.java
+++ project/JLib/src/x/core/ui/JAnimHelper.java
@@ -1,60 +1,17 @@
 package x.core.ui;
 
 import android.os.Handler;
-import android.os.Message;
 import android.os.SystemClock;
-import android.util.Log;
 import android.view.animation.*;
 import android.view.animation.Animation.AnimationListener;
 import android.widget.ViewFlipper;
 
 public class JAnimHelper {
-	private static Animation in, out;
 	private static AnimListener al = new AnimListener();
 	
-	private static void createAnim(int animType) {
-		final int T = Animation.RELATIVE_TO_SELF;
-		final int M = 500;
-		int mask = AnimType.getMask(animType);
-		in = null;
-		out = null;
-		
-		if ((mask & AnimType.MASK_LEFT_IN) != 0) {
-			in = new TranslateAnimation(T, -1, T, 0, T, 0, T, 0);
-			in.setDuration(M);
-		}
-		if ((mask & AnimType.MASK_LEFT_OUT) != 0) {
-			out = new TranslateAnimation(T, 0, T, -1, T, 0, T, 0);
-			out.setDuration(M);
-		}
-		if ((mask & AnimType.MASK_RIGHT_IN) != 0) {
-			in = new TranslateAnimation(T, 1, T, 0, T, 0, T, 0);
-			in.setDuration(M);
-		}
-		if ((mask & AnimType.MASK_RIGHT_OUT) != 0) {
-			out = new TranslateAnimation(T, 0, T, 1.0f, T, 0, T, 0);
-			out.setDuration(M);
-		}
-		if ((mask & AnimType.MASK_TOP_IN) != 0) {
-			in = new TranslateAnimation(T, 0, T, 0, T, -1, T, 0);
-			in.setDuration(M);
-		}
-		if ((mask & AnimType.MASK_TOP_OUT) != 0) {
-			out = new TranslateAnimation(T, 0, T, 0, T, 0, T, -1);
-			out.setDuration(M);
-		}
-		if ((mask & AnimType.MASK_BOTTOM_IN) != 0) {
-			in = new TranslateAnimation(T, 0, T, 0, T, 1, T, 0);
-			in.setDuration(M);
-		}
-		if ((mask & AnimType.MASK_BOTTOM_OUT) != 0) {
-			out = new TranslateAnimation(T, 0, T, 0, T, 0, T, 1);
-			out.setDuration(M);
-		}
-	}
-	
 	public static void installAnim(JPageMgr mgr, ViewFlipper flip, int animType) {
-		createAnim(animType);
+		Animation in = AnimType.createInAnim(animType);
+		Animation out = AnimType.createOutAnim(animType);;
 		flip.setInAnimation(in);
 		flip.setOutAnimation(out);
 		al.reset();
