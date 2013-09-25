e3b8945ff2911a37b94d156a4bdefbedb3f2a151
Gao.Yan <gaoyan@pset.suntect.net>
2013-09-25 18:10:14 +0800

    update NiPage

--- project/TestXLib/jni/NiPage.cpp
+++ project/TestXLib/jni/NiPage.cpp
@@ -15,12 +15,6 @@ NiPage::NiPage(void *pRealPage, const string &name, int id)
 void NiPage::OnClick(const string &id)
 {
     if (id == "go_world") {
-        XFile f("ni.dat");
-        FILE *pf = fopen(f.GetPath().c_str(), "rb+");
-        const char *str = "Hello ni page\n";
-        fwrite(str, strlen(str), 1, pf);
-        fclose(pf);
-
         XPageMgr::Instance()->LoadNewPage("WorldPage", XPageMgr::A_TOP_IN|XPageMgr::A_BOTTOM_OUT);
     }
 }
